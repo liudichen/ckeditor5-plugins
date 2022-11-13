3d2597eaa7228f344260c7bdb57afddde2f568e5
liudichen <liudichen@foxmail.com>
2022-11-13 16:14:40 +0800

    使用eslint-config-egg 替代prettier等

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,22 @@
-/**
- * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
- */
-
-/* eslint-env node */
-
 'use strict';
 
 module.exports = {
-	env: {
-		browser: true,
-		node: true,
-	},
-	parserOptions: {
-		ecmaVersion: 2020,
-		sourceType: 'module',
-	},
-	extends: ['eslint:recommended', 'prettier'],
-	rules: {
-		'prettier/prettier': ['error'],
-		'no-useless-constructor': 'off',
-	},
+  extends: 'eslint-config-egg',
+  parser: '@babel/eslint-parser',
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    requireConfigFile: false,
+  },
+  rules: {
+    'no-useless-constructor': 'off',
+    'generator-star-spacing': 'off',
+    'babel/generator-star-spacing': 'off',
+    'arrow-parens': [ 'warn', 'always' ],
+    'prefer-promise-reject-errors': 'off',
+  },
 };
