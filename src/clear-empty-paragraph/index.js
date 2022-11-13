38c4424806f1d005d2df03f91a1129f5bb8be29b
liudichen <liudichen@foxmail.com>
2022-11-13 16:42:19 +0800

    增加注释

--- src/clear-empty-paragraph/index.js
+++ src/clear-empty-paragraph/index.js
@@ -7,6 +7,7 @@ import { ClearEmptyPragraphEditing } from './editing';
 
 const ATTRIBUTE = 'clearEmptyParagraph';
 
+/** 清除所有空内容的段落,注意会清除掉手动创建的空行, Plugin: ClearEmptyParagraph, toolbar:clearEmptyParagraph */
 export default class ClearEmptyParagraph extends Plugin {
   /**
 	 * @inheritDoc
