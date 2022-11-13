97874ab6473e65ee4405c65d38c5740b69b97861
liudichen <liudichen@foxmail.com>
2022-11-13 16:27:09 +0800

    完善类型注释,修改了参数名称以符合语义 findFirst第3个参数值支持传数组了

--- src/utils.js
+++ src/utils.js
@@ -1,54 +1,55 @@
 export const EXCLIDEBLOCK = 'imageBlock';
 
 /**
- * Find the first successful validation object
+ * Find the first successful validation object 找到第1个满足条件的block对象
  *
- * @param iterator
- * @param success
- * @param nameBlock
- * @returns {null|*}
+ * @param {IterableIterator} iterator selectedBlocks 选择区域可迭代对象
+ * @param {() => boolean} validateFn function to check block is valid 检查block是否符合的函数
+ * @param {string | string[]} excludeBlock excluded block name(s)  排除的block name
+ * @return {null|*} 返回第1个区块
  */
-export function findFirst(iterator, success, nameBlock) {
-	const item = iterator.next();
+export function findFirst(iterator, validateFn, excludeBlock) {
+  const item = iterator.next();
 
-	if (item.done) {
-		return null;
-	}
+  if (item.done) {
+    return null;
+  }
 
-	const value = item.value;
-	if (nameBlock && value.name === nameBlock) {
-		return findFirst(iterator, success, nameBlock);
-	}
-	if (success(value)) {
-		return value;
-	}
+  const value = item.value;
+  if (excludeBlock && (typeof excludeBlock === 'string' && value.name === excludeBlock || Array.isArray(excludeBlock) && excludeBlock.includes(value.name))) {
+    return findFirst(iterator, validateFn, excludeBlock);
+  }
+  if (validateFn(value)) {
+    return value;
+  }
 
-	return findFirst(iterator, success);
+  return findFirst(iterator, validateFn);
 }
 
 /**
- * Find the position (interval) of a text/word in a string
+ * Find the positions (interval) of a text/word in a string
  *
- * @param {string} word target word
- * @param {string} text target string text
- * @param {boolean} caseSensitive
+ * @param {string} word target word 要查找的单词/文本
+ * @param {string} text target string text 搜索范围文本
+ * @param {boolean} caseSensitive caseSensitive? 大小写敏感?
+ * @return {number[]} array of indexs of find word in the string 在字符串中找到对应的index的数组
  */
 export function findIndicesOf(word, text, caseSensitive) {
-	const searchStrLen = word.length;
-	if (searchStrLen === 0) {
-		return [];
-	}
+  const searchStrLen = word.length;
+  if (searchStrLen === 0) {
+    return [];
+  }
 
-	let startIndex = 0;
-	let index;
-	const indices = [];
-	if (!caseSensitive) {
-		text = text.toLowerCase();
-		word = word.toLowerCase();
-	}
-	while ((index = text.indexOf(word, startIndex)) > -1) {
-		indices.push(index);
-		startIndex = index + searchStrLen;
-	}
-	return indices;
+  let startIndex = 0;
+  let index;
+  const indices = [];
+  if (!caseSensitive) {
+    text = text.toLowerCase();
+    word = word.toLowerCase();
+  }
+  while ((index = text.indexOf(word, startIndex)) > -1) {
+    indices.push(index);
+    startIndex = index + searchStrLen;
+  }
+  return indices;
 }
