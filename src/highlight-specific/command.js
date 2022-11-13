94eed359e01892064ae2d04e74ead57c4a4f4dbc
liudichen <liudichen@foxmail.com>
2022-11-13 17:37:53 +0800

    增加export default

--- src/highlight-specific/command.js
+++ src/highlight-specific/command.js
@@ -2,67 +2,67 @@ import { Command } from '@ckeditor/ckeditor5-core';
 import { updateResultFromRange } from './utils';
 
 export class HighlightSpecificCommand extends Command {
-	/**
+  /**
 	 * @inheritDoc
 	 *
-	 * @param {module:core/editor/editor~Editor} editor
-	 * @param state
+	 * @param {module:core/editor/editor~Editor} editor Editor
+	 * @param {object} state state
 	 */
-	constructor(editor, state) {
-		super(editor);
+  constructor(editor, state) {
+    super(editor);
 
-		this.isEnabled = true;
+    this.isEnabled = true;
 
-		// 不能影响数据，须开启只读模式
-		// https://ckeditor.com/docs/ckeditor5/latest/api/module_core_command-Command.html#member-affectsData
-		this.affectsData = false;
+    // 不能影响数据，须开启只读模式
+    // https://ckeditor.com/docs/ckeditor5/latest/api/module_core_command-Command.html#member-affectsData
+    this.affectsData = false;
 
-		this._state = state;
-	}
+    this._state = state;
+  }
 
-	/**
-	 * @param {Object} options
-	 * @param {Array<string>} options.words
-	 * @param {string} [options.color]
+  /**
+	 * @param {Object} options options
+	 * @param {Array<string>} options.words options.words
+	 * @param {string} [options.color] [options.color]
 	 * @fires execute
 	 */
-	execute(options) {
-		if (!(options?.words instanceof Array) || !options?.words.length) {
-			return;
-		}
-		const { editor } = this;
-		const { model } = editor;
-		const { words, color = 'yellow' } = options;
+  execute(options) {
+    if (!(options?.words instanceof Array) || !options?.words.length) {
+      return;
+    }
+    const { editor } = this;
+    const { model } = editor;
+    const { words, color = 'yellow' } = options;
 
-		const config = editor.config.get('highlightSpecific');
+    const config = editor.config.get('highlightSpecific');
 
-		if (!config[color]) {
-			console.warn(`The "${color}" color is not defined, this highlight operate will be ignore!`);
-			return;
-		}
+    if (!config[color]) {
+      console.warn(`The "${color}" color is not defined, this highlight operate will be ignore!`);
+      return;
+    }
 
-		if (this._state[color].length) {
-			this._state.clear(model, color);
-		}
+    if (this._state[color].length) {
+      this._state.clear(model, color);
+    }
 
-		words.forEach((word) => {
-			if (!word) {
-				return;
-			}
-			const results = model.document.getRootNames().reduce((currResult, rootName) => {
-				return updateResultFromRange(
-					model.createRangeIn(model.document.getRoot(rootName)),
-					model,
-					currResult,
-					word,
-					color
-				);
-			}, null);
+    words.forEach((word) => {
+      if (!word) {
+        return;
+      }
+      const results = model.document.getRootNames().reduce((currResult, rootName) => {
+        return updateResultFromRange(
+          model.createRangeIn(model.document.getRoot(rootName)),
+          model,
+          currResult,
+          word,
+          color
+        );
+      }, null);
 
-			this._state[color].addMany(Array.from(results));
-		});
+      this._state[color].addMany(Array.from(results));
+    });
 
-		/*const results = */
-		// console.warn(results);
-	}
+    /* const results = */
+    // console.warn(results);
+  }
 }
--- src/highlight-specific/index.js
+++ src/highlight-specific/index.js
@@ -2,13 +2,13 @@ import { Plugin } from '@ckeditor/ckeditor5-core';
 import { HighlightSpecificEditing } from './editing';
 
 export class HighlightSpecific extends Plugin {
-	static get requires() {
-		return [HighlightSpecificEditing];
-	}
+  static get requires() {
+    return [ HighlightSpecificEditing ];
+  }
 
-	static get pluginName() {
-		return 'HighlightSpecific';
-	}
+  static get pluginName() {
+    return 'HighlightSpecific';
+  }
 }
 
 /**
